11f19520a0eeb4b9778435b9afa40e12413cd6ee
Qohat <qohatpp@gmail.com>
2022-07-11 08:31:48 -0500

    Inherit transform

--- src/main/scala/Streams.scala
+++ src/main/scala/Streams.scala
@@ -2,9 +2,17 @@ import akka.NotUsed
 import akka.stream.scaladsl.Source
 import akka.actor.ActorSystem
 
-sealed trait Message
-case class Message1(a: String, b: String) extends Message
-case class Message2(a: String, b: String, c: Option[String]) extends Message
+sealed trait Message {
+  def toEmailMessage: EmailMessage
+}
+case class Message1(a: String, b: String) extends Message {
+  override def toEmailMessage: EmailMessage =
+    Welcome(a, b)
+}
+case class Message2(a: String, b: String, c: Option[String]) extends Message {
+  override def toEmailMessage: EmailMessage =
+    NewAccount(a, b, c.getOrElse(""))
+}
 
 sealed trait EmailMessage {
   val id: String
@@ -16,7 +24,7 @@ case class NewAccount(id: String, email: String, firstName: String) extends Emai
 
 sealed trait Queue
 
-trait Consumer[-T] {
+trait Consumer {
   def consume(q: Queue): Unit
 }
 
@@ -25,17 +33,18 @@ object Consumer {
 
   val messages: Source[Message, NotUsed] = Source(Message1("a", "b") :: Message2("a", "b", None) :: Nil)
 
-  def make[T]: Consumer[T] =
-    new Consumer[T]:
+  def make: Consumer =
+    new Consumer:
       override def consume(q: Queue): Unit =
         messages
+          .map(_.toEmailMessage)
           .runForeach(println)
 }
 
 object MainStreams {
   def main(args: Array[String]): Unit =
     Consumer
-      .make[Message1]
+      .make
       .consume(new Queue {})
 }
 
